02718e78dce2ef9ae9cb7c166529d168d74f2406
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-19 23:33:02 -0700

    update

--- csv_to_json.py
+++ csv_to_json.py
@@ -6,14 +6,7 @@ if len(args) < 2:
     print("python3 csv_to_json.py [csv file] #convert pop file to json in expected way")
     sys.exit(1)
 
-d = open(args[1]).read()
-
+fn = args[1]
+d = open(fn).read()
 dat = '{"x":"' + d + '")'
-
-open(args[1] + ".json", "wb").write(dat.encode())
-
-
-
-
-
-
+open(fn[:-4] + ".json", "wb").write(dat.encode())
