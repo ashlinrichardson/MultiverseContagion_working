960aa873b5e4ff7421251bdf8ee0af0c7925fdaf
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-04-19 23:35:30 -0700

    update

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,10 +1,19 @@
+'''convert all csv files in the present folder, to the json form expected by CovidSIM
+'''
 import os
 import sys
 args = sys.argv
 
 if len(args) < 2:
-    print("python3 csv_to_json.py [csv file] #convert pop file to json in expected way")
-    sys.exit(1)
+    files = os.popen('ls -1 *.csv').readlines()
+    if len(files) < 1:
+        print("python3 csv_to_json.py [csv file] #convert pop file to json in expected way")
+        sys.exit(1)
+    else:
+        files = [x.strip() for x in files]
+        for f in files:
+            a = os.system('python3 csv_to_json.py ' + f)
+        sys.exit(0)
 
 fn = args[1]
 d = open(fn).read()
